cdf8240590c07e5aad9d369dbc2790e323a57217
Jonas Avelino <jonasavma@gmail.com>
2024-02-06 16:19:35 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,11 @@
 package aplication;
 
+
+
+import java.util.Date;
+
 import model.Department;
+import model.Seller;
 
 public class Program {
 
@@ -8,7 +13,9 @@ public class Program {
 
 		Department obj = new Department(1,"Books");
 		
-		System.out.println(obj);
+		Seller seller=new Seller(21,"Bob","bob@gmail.com",new Date(),30000.00,obj);
+		
+		System.out.println(seller);
 		
 		
 		
