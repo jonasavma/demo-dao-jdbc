74f931dd1f2fb8cdabbdd7e3e08c0fad1c9e3a15
Jonas Avelino <jonasavma@gmail.com>
2024-02-06 16:43:13 -0300

    SellerDaoJDBC and DaoFactory

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,9 @@
 package aplication;
 
-
-
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entidade.Department;
 import model.entidade.Seller;
 
@@ -11,14 +11,14 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1,"Books");
-		
-		Seller seller=new Seller(21,"Bob","bob@gmail.com",new Date(),30000.00,obj);
-		
+		Department obj = new Department(1, "Books");
+
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 30000.00, obj);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		System.out.println(seller);
-		
-		
-		
+
 	}
 
 }
