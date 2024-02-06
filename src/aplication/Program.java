4aee433199ebe590d7291f858448b89d4e0f8f46
Jonas Avelino <jonasavma@gmail.com>
2024-02-06 16:07:14 -0300

    Department entity class
    correção da pasta local

---
+++ src/aplication/Program.java
@@ -0,0 +1,17 @@
+package aplication;
+
+import model.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1,"Books");
+		
+		System.out.println(obj);
+		
+		
+		
+	}
+
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module jdbc1 {
-	requires java.sql;
-}
